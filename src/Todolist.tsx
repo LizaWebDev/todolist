48c535627eaef47f5900952258aed1c981a70f4e
LizaWebDev <e.kuntsevich03@gmail.com>
2024-05-24 11:57:50 +0300

    Button component, Array<TaskType> and some improvements

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import {Button} from "./Button";
 
-type TaskType = {
+export type TaskType = {
     id: number
     title: string
     isDone: boolean
@@ -17,17 +18,26 @@ export function Todolist(props: PropsType) {
             <h3>{props.title}</h3>
             <div>
                 <input/>
-                <button>+</button>
+                <Button title={'+'} />
             </div>
-            <ul>
-                <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
-                <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-                <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
-            </ul>
+            {props.tasks.length === 0 ? (
+                <p>Тасок нет</p>
+            ) : (
+                <ul>
+                    {props.tasks.map(task => {
+                        return (
+                            <li key={task.id}>
+                                <input type="checkbox" checked={task.isDone} readOnly={true} />
+                                <span>{task.title}</span>
+                            </li>
+                        );
+                    })}
+                </ul>
+            )}
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <Button title={'All'} />
+                <Button title={'Active'} />
+                <Button title={'Completed'} />
             </div>
         </div>
     );
